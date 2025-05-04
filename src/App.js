0bb155e3974bf86899a811340f0ceafc8eaf786e
Mohd Sameer <83845532+sameer3134@users.noreply.github.com>
2025-05-04 06:44:31 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   
 <footer className="page-footer font-large blue">
   <div className="footer-copyright text-center py-3 text-primary ">
-    <a > :-Designed by Mohd Sameer</a>
+    <a > :-Designed by me</a>
   </div>
 </footer>
 
